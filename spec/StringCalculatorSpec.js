d449a202d0fff862cb8f6f7bce92ef7766f3798b
Darren Sim <dasimsg@gmail.com>
2018-05-30 08:04:42 +1200

    Refactor

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -6,15 +6,6 @@ describe("String Calculator", function(){
         stringCalculator = require("../src/StringCalculator");
     });
 
-    it("has an add function that takes in a string value and returns a number result", function(){
-        const input = "1";
-        const expectedResult = 0;
-
-        const result = stringCalculator.add(input);
-
-        expect(result).toBe(expectedResult);
-    });
-
     it("has an add function that returns 0 when an empty string input is entered", function(){
         const input = "";
         const expectedResult = 0;
