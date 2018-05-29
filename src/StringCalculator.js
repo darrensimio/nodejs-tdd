e19206c24c50ec3d3e8154240b191c8cec9eea53
Darren Sim <dasimsg@gmail.com>
2018-05-30 08:18:58 +1200

    1b: Passing Test

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,14 @@
 module.exports = {
         add: function (inputValue) {
-                if (!inputValue) return 0;
-                else return 1;
+                if (!inputValue) {
+                        return 0;
+                } else if (inputValue.indexOf(',') < 0) {
+                        return parseInt(inputValue, 10);
+                } else {
+                        var inputValues = inputValue.split(',');
+                        return inputValues.reduce(function (acc, val) {
+                                return parseInt(acc, 10) + parseInt(val, 10);
+                        });
+                }
         },
 };
